4d9d68b3a8b2f40095402b71f1a4d9992f71d8b6
ZjzMisaka <ZjzMisaka@iaders.com>
2023-08-26 03:52:00 +0800

    +more generic support

--- PowerThreadPool/PowerThreadPool.cs
+++ PowerThreadPool/PowerThreadPool.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Concurrent;
+using System.Xml.Linq;
 
 namespace PowerThreadPool
 {
@@ -42,11 +43,74 @@ namespace PowerThreadPool
             QueueWorkItem<object>(func, param, callBack);
         }
 
+        public void QueueWorkItem<T1>(Action<T1> action, T1 param1, Action<ExcuteResult<object>> callBack = null)
+        {
+            Func<T1, object> func = (param) => { action(param1); return null; };
+            QueueWorkItem<T1, object>(func, param1, callBack);
+        }
+
+        public void QueueWorkItem<T1, T2>(Action<T1, T2> action, T1 param1, T2 param2, Action<ExcuteResult<object>> callBack = null)
+        {
+            Func<T1, T2, object> func = (param1, param2) => { action(param1, param2); return null; };
+            QueueWorkItem<T1, T2, object>(func, param1, param2, callBack);
+        }
+
+        public void QueueWorkItem<T1, T2, T3>(Action<T1, T2, T3> action, T1 param1, T2 param2, T3 param3, Action<ExcuteResult<object>> callBack = null)
+        {
+            Func<T1, T2, T3, object> func = (param1, param2, param3) => { action(param1, param2, param3); return null; };
+            QueueWorkItem<T1, T2, T3, object>(func, param1, param2, param3, callBack);
+        }
+
+        public void QueueWorkItem<T1, T2, T3, T4>(Action<T1, T2, T3, T4> action, T1 param1, T2 param2, T3 param3, T4 param4, Action<ExcuteResult<object>> callBack = null)
+        {
+            Func<T1, T2, T3, T4, object> func = (param1, param2, param3, param4) => { action(param1, param2, param3, param4); return null; };
+            QueueWorkItem<T1, T2, T3, T4, object>(func, param1, param2, param3, param4, callBack);
+        }
+
+        public void QueueWorkItem<T1, T2, T3, T4, T5>(Action<T1, T2, T3, T4, T5> action, T1 param1, T2 param2, T3 param3, T4 param4, T5 param5, Action<ExcuteResult<object>> callBack = null)
+        {
+            Func<T1, T2, T3, T4, T5, object> func = (param1, param2, param3, param4, param5) => { action(param1, param2, param3, param4, param5); return null; };
+            QueueWorkItem<T1, T2, T3, T4, T5, object>(func, param1, param2, param3, param4, param5, callBack);
+        }
+
+
+        public void QueueWorkItem<T1, TResult>(Func<T1, TResult> function, T1 param1, Action<ExcuteResult<TResult>> callBack = null)
+        {
+            Func<TResult> func = () => { return function(param1); };
+            QueueWorkItem<TResult>(func, callBack);
+        }
+
+        public void QueueWorkItem<T1, T2, TResult>(Func<T1, T2, TResult> function, T1 param1, T2 param2, Action<ExcuteResult<TResult>> callBack = null)
+        {
+            Func<TResult> func = () => { return function(param1, param2); };
+            QueueWorkItem<TResult>(func, callBack);
+        }
+
+        public void QueueWorkItem<T1, T2, T3, TResult>(Func<T1, T2, T3, TResult> function, T1 param1, T2 param2, T3 param3, Action<ExcuteResult<TResult>> callBack = null)
+        {
+            Func<TResult> func = () => { return function(param1, param2, param3); };
+            QueueWorkItem<TResult>(func, callBack);
+        }
+
+        public void QueueWorkItem<T1, T2, T3, T4, TResult>(Func<T1, T2, T3, T4, TResult> function, T1 param1, T2 param2, T3 param3, T4 param4, Action<ExcuteResult<TResult>> callBack = null)
+        {
+            Func<TResult> func = () => { return function(param1, param2, param3, param4); };
+            QueueWorkItem<TResult>(func, callBack);
+        }
+
+        public void QueueWorkItem<T1, T2, T3, T4, T5, TResult>(Func<T1, T2, T3, T4, T5, TResult> function, T1 param1, T2 param2, T3 param3, T4 param4, T5 param5, Action<ExcuteResult<TResult>> callBack = null)
+        {
+            Func<TResult> func = () => { return function(param1, param2, param3, param4, param5); };
+            QueueWorkItem<TResult>(func, callBack);
+        }
+
+
         public void QueueWorkItem<TResult>(Func<TResult> function, Action<ExcuteResult<TResult>> callBack = null)
         {
             Func<object[], TResult> func = (param) => { return function(); };
             QueueWorkItem<TResult>(func, new object[0], callBack);
         }
+
 
         public void QueueWorkItem<TResult>(Func<object[], TResult> function, object[] param, Action<ExcuteResult<TResult>> callBack = null)
         {
