893104421bbeb75094db8711dd5cc59c9e2ebe12
ZjzMisaka <530514257@qq.com>
2024-02-26 00:01:53 +0800

    update test

--- PowerThreadPoolTest/MainWindow.xaml.cs
+++ PowerThreadPoolTest/MainWindow.xaml.cs
@@ -27,56 +27,92 @@ namespace PowerThreadPoolTest
     public partial class MainWindow : Window
     {
         PowerPool powerPool = new PowerPool();
-        string t4Guid = "";
-        string t2Guid = "";
-        System.Timers.Timer timer = new System.Timers.Timer(100);
-        string msg = "";
+        Random random = new Random();
+        bool run = false;
+        int doneCount = 0;
+
         public MainWindow()
         {
             InitializeComponent();
 
-            timer.Elapsed += (s, e) =>
-            {
-                OutputCount();
-            };
-            timer.Start();
-
             powerPool.PowerPoolOption = new PowerPoolOption()
             {
                 MaxThreads = 8,
+                DestroyThreadOption = new DestroyThreadOption() { MinThreads = 4, KeepAliveTime = 1000 },
+                StartSuspended = true,
                 DefaultCallback = (res) =>
                 {
-                    OutputMsg("DefaultCallback");
+                    Interlocked.Increment(ref doneCount);
                 },
-                DestroyThreadOption = new DestroyThreadOption() { MinThreads = 4, KeepAliveTime = 3000 }
             };
-
-            powerPool.PoolStart += (s, e) =>
-            {
-                log.Text = "";
-                OutputMsg("PoolStart");
-            };
-            powerPool.PoolIdle += (s, e) =>
-            {
-                OutputMsg("PoolIdle");
-            };
-            powerPool.WorkStart += (s, e) =>
-            {
-                OutputMsg("WorkStart");
-            };
-            powerPool.WorkEnd += (s, e) =>
-            {
-                OutputMsg("WorkEnd");
-            };
-            powerPool.WorkTimeout += (s, e) =>
-            {
-                OutputMsg("Thread" + e.ID + ": Timeout");
-            };
-            powerPool.ForceStop += (s, e) =>
-            {
-                OutputMsg("Thread" + e.ID + ": ForceStop");
-            };
+            powerPool.PoolStart += (s, e) => { OutputMsg("PoolStart"); };
+            powerPool.PoolIdle += (s, e) => { OutputMsg("PoolIdle"); };
         }
+
+        private async void start_Click(object sender, RoutedEventArgs e)
+        {
+            start.IsEnabled = false;
+
+            run = true;
+            while (run)
+            {
+                int runCount = random.Next(10, 200);
+                doneCount = 0;
+                for (int i = 0; i < runCount; ++i)
+                {
+                    int r =random.Next(0, 101);
+                    if (r == 100)
+                    {
+                        powerPool.QueueWorkItem(() => { throw new Exception(); });
+                    }
+                    else if(r >= 95 && r <= 99)
+                    {
+                        powerPool.QueueWorkItem(() => { Sleep(10000); });
+                    }
+                    else if (r >= 94 && r <= 94)
+                    {
+                        powerPool.QueueWorkItem(() => { Sleep(30000); });
+                    }
+                    else
+                    {
+                        powerPool.QueueWorkItem(() => { Sleep(random.Next(500, 1000)); });
+                    }
+                }
+                OutputMsg("Run count: " + runCount);
+                OutputMsg("WaitingWorkCount: " + powerPool.WaitingWorkCount);
+                if (runCount != powerPool.WaitingWorkCount)
+                {
+                    break;
+                }
+
+                powerPool.Start();
+
+                await powerPool.WaitAsync();
+                OutputMsg("RunningWorkerCount: " + powerPool.RunningWorkerCount);
+                OutputMsg("WaitingWorkCount: " + powerPool.WaitingWorkCount);
+                OutputMsg("FailedWorkCount: " + powerPool.FailedWorkCount);
+                OutputMsg("DoneCount: " + doneCount);
+                OutputMsg("---------------");
+                if (powerPool.RunningWorkerCount > 0 || powerPool.WaitingWorkCount > 0 || runCount != doneCount)
+                {
+                    break;
+                }
+                Sleep(random.Next(0, 1500));
+            }
+            OutputMsg("done");
+
+            start.IsEnabled = true;
+        }
+
+        private void stop_Click(object sender, RoutedEventArgs e)
+        {
+            run = false;
+            powerPool.Stop();
+        }
+
+
+
+
 
         private void Sleep(int ms)
         {
@@ -84,343 +120,35 @@ namespace PowerThreadPoolTest
 
             while (stopwatch.ElapsedMilliseconds < ms)
             {
+                powerPool.StopIfRequested();
             }
 
             stopwatch.Stop();
         }
 
-        private void stop_Click(object sender, RoutedEventArgs e)
-        {
-            powerPool.Stop();
-        }
-
-        private void forceStop_Click(object sender, RoutedEventArgs e)
-        {
-            powerPool.Stop(true);
-        }
-
-        private void stopThread2_Click(object sender, RoutedEventArgs e)
-        {
-            if (powerPool.Stop(t2Guid))
-            {
-                OutputMsg("stopThread2 succees");
-            }
-            else 
-            {
-                OutputMsg("stopThread2 failed");
-            }
-        }
-
-        private async void wait_Click(object sender, RoutedEventArgs e)
-        {
-            wait.IsEnabled = false;
-            await powerPool.WaitAsync();
-            OutputMsg("ALL Thread End");
-            wait.IsEnabled = true;
-        }
-
-        private void pause_Click(object sender, RoutedEventArgs e)
-        {
-            powerPool.Pause();
-        }
-
-        private void resume_Click(object sender, RoutedEventArgs e)
-        {
-            powerPool.Resume(true);
-        }
-
-        private void pauseThread2_Click(object sender, RoutedEventArgs e)
-        {
-            powerPool.Pause(t2Guid);
-        }
-
-        private void resumeThread2_Click(object sender, RoutedEventArgs e)
-        {
-            powerPool.Resume(t2Guid);
-        }
-
-        private void cancelThread4_Click(object sender, RoutedEventArgs e)
-        {
-            if (powerPool.Cancel(t4Guid))
-            {
-                OutputMsg("Cancel succeed");
-            }
-            else
-            {
-                OutputMsg("Cancel failed");
-            }
-        }
-
-        private void cancelAllThread_Click(object sender, RoutedEventArgs e)
-        {
-            powerPool.Cancel();
-        }
-
         private void OutputMsg(string msg)
-        {
-            this.msg += msg + "\n";
-        }
-
-        private void OutputCount()
         {
             this.Dispatcher.Invoke(() =>
             {
-                if (msg != "")
-                {
-                    log.Text += msg;
-                    msg = "";
-                    sv.ScrollToEnd();
-                }
-
-                string countTxt =
-                "WaitingWorkCount: " + powerPool.WaitingWorkCount + "\n" +
-                "RunningWorkerCount: " + powerPool.RunningWorkerCount + "\n" +
-                "IdleWorkerCount: " + powerPool.IdleWorkerCount + "\n" +
-                "Running: " + powerPool.PoolRunning.ToString() + "\n" +
-                "Stopping: " + powerPool.PoolStopping.ToString();
-                count.Text = countTxt;
+                log.Text += msg + "\n";
+                log.Text = GetLastThousandLines(log.Text);
+                sv.ScrollToEnd();
             });
         }
 
-
-
-
-
-
-
-        private void start_Click(object sender, RoutedEventArgs e)
+        private string GetLastThousandLines(string input)
         {
-            powerPool.QueueWorkItem(() =>
+            string[] lines = input.Split(new string[] { Environment.NewLine }, StringSplitOptions.None);
+
+            if (lines.Length <= 1000)
             {
-                OutputMsg("Thread0: START");
-                Sleep(10000);
-                powerPool.PauseIfRequested();
-                powerPool.StopIfRequested();
-                OutputMsg("Thread0: END");
-
-                return new Exception("11223344");
-            });
-
-            powerPool.QueueWorkItem(() => 
-            {
-                for (int i = 0; i < 20; ++i)
-                {
-                    powerPool.PauseIfRequested();
-                    powerPool.StopIfRequested();
-                    OutputMsg("Thread1: " + i.ToString());
-                    Sleep(1000);
-                }
-                OutputMsg("Thread1: END");
-                return true;
-            }, new WorkOption()
-            {
-                Callback = (res) =>
-                {
-                    OutputMsg("Thread1: Callback");
-                },
-                Timeout = new TimeoutOption() { Duration = 2000 }
-            });
-
-            t2Guid = powerPool.QueueWorkItem(() =>
-            {
-                for (int i = 0; i < 20; ++i)
-                {
-                    powerPool.PauseIfRequested();
-                    powerPool.StopIfRequested();
-                    OutputMsg("Thread2: " + i.ToString());
-                    Sleep(700);
-                }
-                OutputMsg("Thread2: END");
-            });
-
-            powerPool.QueueWorkItem(() =>
-            {
-                for (int i = 0; i < 20; ++i)
-                {
-                    powerPool.PauseIfRequested();
-                    powerPool.StopIfRequested();
-                    OutputMsg("Thread3: " + i.ToString());
-                    Sleep(500);
-                }
-                OutputMsg("Thread3: END");
-                return new PowerPoolOption();
-            }, (res) =>
-            {
-                OutputMsg("Thread3: Callback");
-            });
-
-            t4Guid = powerPool.QueueWorkItem(() =>
-            {
-                for (int i = 0; i < 20; ++i)
-                {
-                    powerPool.PauseIfRequested();
-                    powerPool.StopIfRequested();
-                    OutputMsg("Thread4: " + i.ToString());
-                    Sleep(500);
-                }
-                OutputMsg("Thread4: END");
-            }, new WorkOption()
-            {
-                Callback = (res) =>
-                {
-                    OutputMsg("Thread4: Callback");
-                },
-                WorkPriority = 10
-            });
-
-            powerPool.QueueWorkItem<int, int, Random>(T5Func, 10, 10, T5Callback);
-            powerPool.QueueWorkItem<int, int>(T6Action, 10, 10);
-        }
-
-        private Random T5Func(int x, int y)
-        {
-            OutputMsg("T5Func: x + y :" + (x + y).ToString());
-            return new Random();
-        }
-
-        private void T5Callback(ExecuteResult<Random> res)
-        {
-            OutputMsg("Random :" + res.Result.Next());
-        }
-
-        private void T6Action(int x, int y)
-        {
-            OutputMsg("T6Action: x + y :" + (x + y).ToString());
-        }
-
-
-
-
-
-
-        private async void Window_Loaded(object sender, RoutedEventArgs e)
-        {
-            if (MessageBox.Show("Run Stress Test1?", "", MessageBoxButton.YesNo) == MessageBoxResult.Yes)
-            {
-                object lockObj = new object();
-                for (int i = 0; i < 10; ++i)
-                {
-                    int doneCount = 0;
-                    PowerPool powerPool = new PowerPool(new PowerPoolOption() { });
-
-                    Task[] tasks = Enumerable.Range(0, 100000).Select(i =>
-                        Task.Run(() =>
-                        {
-                            string workId = powerPool.QueueWorkItem(() =>
-                            {
-                            }, (res) =>
-                            {
-                                lock (lockObj)
-                                {
-                                    ++doneCount;
-                                }
-                            });
-                        }
-                        )
-                    ).ToArray();
-
-                    await Task.WhenAll(tasks);
-                    await powerPool.WaitAsync();
-                }
-
-                MessageBox.Show("OK");
-                this.Close();
+                return input;
             }
-
-            if (MessageBox.Show("Run Stress Test2?", "", MessageBoxButton.YesNo) == MessageBoxResult.Yes)
+            else
             {
-                object lockObj = new object();
-                PowerPool powerPool = new PowerPool(new PowerPoolOption() { });
-                int startCount = 0;
-                int idleCount = 0;
-                int doneCount = 0;
-
-                powerPool.PoolStart += (s, e) =>
-                {
-                    lock (lockObj)
-                    {
-                        ++startCount;
-                    }
-                };
-                powerPool.PoolIdle += (s, e) =>
-                {
-                    lock (lockObj)
-                    {
-                        ++idleCount;
-
-                    }
-                };
-
-                for (int i = 0; i < 100; ++i)
-                {
-                    powerPool.QueueWorkItem(() =>
-                    {
-                        for (int j = 0; j < 100; ++j)
-                        {
-                            powerPool.QueueWorkItem(() =>
-                            {
-                                for (int k = 0; k < 100; ++k)
-                                {
-                                    powerPool.QueueWorkItem(() =>
-                                    {
-                                    }, (res) =>
-                                    {
-                                        for (int j = 0; j < 5; ++j)
-                                        {
-                                            powerPool.QueueWorkItem(() =>
-                                            {
-
-                                            }, (res) =>
-                                            {
-                                                lock (lockObj)
-                                                {
-                                                    ++doneCount;
-                                                }
-                                            });
-                                        }
-                                        lock (lockObj)
-                                        {
-                                            ++doneCount;
-                                        }
-                                    });
-                                }
-                            }, (res) =>
-                            {
-                                lock (lockObj)
-                                {
-                                    ++doneCount;
-                                }
-                            });
-                        }
-                    }, (res) =>
-                    {
-                        lock (lockObj)
-                        {
-                            ++doneCount;
-                        }
-                    });
-                }
-
-                while (powerPool.PoolRunning)
-                {
-                    while (powerPool.PoolRunning)
-                    {
-                        while (powerPool.PoolRunning)
-                        {
-                            while (powerPool.PoolRunning)
-                            {
-                                await powerPool.WaitAsync();
-                                Thread.Sleep(5000);
-                            }
-                            Thread.Sleep(100);
-                        }
-                        Thread.Sleep(100);
-                    }
-                    Thread.Sleep(100);
-                }
-
-                MessageBox.Show("OK");
-                this.Close();
+                string[] lastThousandLines = new string[1000];
+                Array.Copy(lines, lines.Length - 1000, lastThousandLines, 0, 1000);
+                return string.Join(Environment.NewLine, lastThousandLines);
             }
         }
     }
