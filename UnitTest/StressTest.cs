c37551788ef8373629ae2ee96697a523b8bc10df
ZjzMisaka <zjzmisaka@outlook.com>
2024-04-19 12:00:14 +0800

    +StressTest4 for LIFO

--- UnitTest/StressTest.cs
+++ UnitTest/StressTest.cs
@@ -224,6 +224,144 @@ namespace UnitTest
             }
         }
 
+        [Fact]
+        public async Task StressTest4()
+        {
+            Random random = new Random();
+            bool run = false;
+            int doneCount = 0;
+
+            powerPool = new PowerPool();
+            powerPool.PowerPoolOption = new PowerPoolOption()
+            {
+                MaxThreads = 8,
+                DestroyThreadOption = new DestroyThreadOption() { MinThreads = 4, KeepAliveTime = 1000 },
+                StartSuspended = true,
+                DefaultCallback = (res) =>
+                {
+                    Interlocked.Increment(ref doneCount);
+                },
+                QueueType = QueueType.LIFO
+            };
+
+            long start = GetNowSs();
+            run = true;
+            while (run)
+            {
+                if (GetNowSs() - start >= 300000)
+                {
+                    break;
+                }
+
+                int runCount = random.Next(10, 200);
+                doneCount = 0;
+                for (int i = 0; i < runCount; ++i)
+                {
+                    int r = random.Next(0, 101);
+                    if (r == 100)
+                    {
+                        string id = powerPool.QueueWorkItem(() => { throw new Exception(); });
+                        if (id == null)
+                        {
+                            Assert.Fail("PoolStopping");
+                        }
+                    }
+                    else if (r >= 95 && r <= 99)
+                    {
+                        string id = powerPool.QueueWorkItem(() =>
+                        {
+                            Sleep(10000);
+                            int r1 = random.Next(0, 101);
+                            if (r1 >= 100 && r1 <= 100)
+                            {
+                                Thread.Sleep(1);
+                            }
+                        });
+                        if (id == null)
+                        {
+                            Assert.Fail("PoolStopping");
+                        }
+                    }
+                    else if (r >= 94 && r <= 94)
+                    {
+                        string id = powerPool.QueueWorkItem(() =>
+                        {
+                            Sleep(30000);
+                            int r1 = random.Next(0, 101);
+                            if (r1 >= 100 && r1 <= 100)
+                            {
+                                Thread.Sleep(1);
+                            }
+                        });
+                        if (id == null)
+                        {
+                            Assert.Fail("PoolStopping");
+                        }
+                    }
+                    else
+                    {
+                        string id = powerPool.QueueWorkItem(() =>
+                        {
+                            Sleep(random.Next(500, 1000));
+                            int r1 = random.Next(0, 101);
+                            if (r1 >= 100 && r1 <= 100)
+                            {
+                                Thread.Sleep(1);
+                            }
+                        });
+                        if (id == null)
+                        {
+                            Assert.Fail("PoolStopping");
+                        }
+                    }
+                }
+
+                Thread.Yield();
+
+                if (runCount != powerPool.WaitingWorkCount)
+                {
+                    Assert.Fail();
+                }
+
+                powerPool.Start();
+
+                int r1 = random.Next(0, 101);
+                if (r1 >= 81 && r1 <= 100)
+                {
+                    await powerPool.StopAsync();
+                    await powerPool.WaitAsync();
+                    if (powerPool.RunningWorkerCount > 0 || powerPool.WaitingWorkCount > 0)
+                    {
+                        Assert.Fail();
+                    }
+                }
+                else if (r1 >= 61 && r1 <= 80)
+                {
+                    await powerPool.StopAsync(true);
+                    await powerPool.WaitAsync();
+                    if (powerPool.RunningWorkerCount > 0 || powerPool.WaitingWorkCount > 0)
+                    {
+                        Assert.Fail();
+                    }
+                }
+                else
+                {
+                    await powerPool.WaitAsync();
+                    if (powerPool.RunningWorkerCount > 0 || powerPool.WaitingWorkCount > 0 || runCount != doneCount)
+                    {
+                        Assert.Fail();
+                    }
+                }
+                if (r1 >= 81 && r1 <= 100)
+                {
+                }
+                else
+                {
+                    Sleep(random.Next(0, 1500));
+                }
+            }
+        }
+
         private void Sleep(int ms)
         {
             Stopwatch stopwatch = Stopwatch.StartNew();
