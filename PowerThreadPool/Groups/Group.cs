b2d2a8a78cebdcf901b795ea582d950fe73accc6
ZjzMisaka <zjzmisaka@outlook.com>
2024-05-11 11:46:15 +0800

    add comments

--- PowerThreadPool/Groups/Group.cs
+++ PowerThreadPool/Groups/Group.cs
@@ -16,11 +16,18 @@ namespace PowerThreadPool.Groups
             this.groupName = groupName;
         }
 
+        /// <summary>
+        /// Wait until all the work belonging to the group is done.
+        /// </summary>
         public void Wait()
         {
             powerPool.Wait(powerPool.GetGroupMemberList(groupName));
         }
 
+        /// <summary>
+        /// Wait until all the work belonging to the group is done.
+        /// </summary>
+        /// <returns></returns>
         public async Task WaitAsync()
         {
             await Task.Run(() =>
@@ -29,21 +36,38 @@ namespace PowerThreadPool.Groups
             });
         }
 
+        /// <summary>
+        /// Stop all the work belonging to the group.
+        /// </summary>
+        /// <param name="forceStop">Call Thread.Interrupt() for force stop</param>
+        /// <returns>Return false if no thread running</returns>
         public List<string> Stop(bool forceStop = false)
         {
             return powerPool.Stop(powerPool.GetGroupMemberList(groupName), forceStop);
         }
 
+        /// <summary>
+        /// Pause all the work belonging to the group.
+        /// </summary>
+        /// <returns>Return a list of IDs for work that doesn't exist</returns>
         public List<string> Pause()
         {
             return powerPool.Pause(powerPool.GetGroupMemberList(groupName));
         }
 
+        /// <summary>
+        /// Resume all the work belonging to the group.
+        /// </summary>
+        /// <returns>Return a list of IDs for work that doesn't exist</returns>
         public List<string> Resume()
         {
             return powerPool.Resume(powerPool.GetGroupMemberList(groupName));
         }
 
+        /// <summary>
+        /// Cancel all the work belonging to the group.
+        /// </summary>
+        /// <returns>Return a list of IDs for work that doesn't exist</returns>
         public List<string> Cancel()
         {
             return powerPool.Cancel(powerPool.GetGroupMemberList(groupName));
