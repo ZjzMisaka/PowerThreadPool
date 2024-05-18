9cb101b2704af6c218223b856d59c517d81300dd
ZjzMisaka <zjzmisaka@outlook.com>
2024-05-18 11:21:07 +0800

    Revert PowerThreadPoolTest

--- PowerThreadPoolTest/MainWindow.xaml.cs
+++ PowerThreadPoolTest/MainWindow.xaml.cs
@@ -4,7 +4,6 @@ using System;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading;
-using System.Threading.Tasks;
 using System.Windows;
 
 namespace PowerThreadPoolTest
@@ -27,11 +26,14 @@ namespace PowerThreadPoolTest
             {
                 MaxThreads = 8,
                 DestroyThreadOption = new DestroyThreadOption() { MinThreads = 4, KeepAliveTime = 1000 },
+                StartSuspended = true,
                 DefaultCallback = (res) =>
                 {
                     Interlocked.Increment(ref doneCount);
                 },
             };
+            powerPool.PoolStarted += (s, e) => { OutputMsg("PoolStart"); };
+            powerPool.PoolIdled += (s, e) => { OutputMsg("PoolIdle"); };
         }
 
         private async void start_Click(object sender, RoutedEventArgs e)
@@ -39,22 +41,150 @@ namespace PowerThreadPoolTest
             start.IsEnabled = false;
 
             run = true;
-            int totalTasks = 1000;
-            for (int i = 0; i < 10000; ++i)
+            while (run)
             {
-                Task[] tasks = Enumerable.Range(0, totalTasks).Select(i =>
-                    Task.Run(() =>
+                int runCount = random.Next(10, 200);
+                doneCount = 0;
+                for (int i = 0; i < runCount; ++i)
+                {
+                    int r =random.Next(0, 101);
+                    if (r == 100)
                     {
-                        powerPool.QueueWorkItem(() =>
-                        {
-                            Interlocked.Increment(ref doneCount);
+                        powerPool.QueueWorkItem(() => { throw new Exception(); });
+                    }
+                    else if(r >= 95 && r <= 99)
+                    {
+                        powerPool.QueueWorkItem(() => 
+                        { 
+                            Sleep(10000);
+                            int r1 = random.Next(0, 101);
+                            if (r1 >= 100 && r1 <= 100)
+                            {
+                                Thread.Sleep(1);
+                            }
                         });
-                    })
-                ).ToArray();
+                    }
+                    else if (r >= 94 && r <= 94)
+                    {
+                        powerPool.QueueWorkItem(() => 
+                        { 
+                            Sleep(30000);
+                            int r1 = random.Next(0, 101);
+                            if (r1 >= 100 && r1 <= 100)
+                            {
+                                Thread.Sleep(1);
+                            }
+                        });
+                    }
+                    else
+                    {
+                        powerPool.QueueWorkItem(() => 
+                        { 
+                            Sleep(random.Next(500, 1000));
+                            int r1 = random.Next(0, 101);
+                            if (r1 >= 100 && r1 <= 100)
+                            {
+                                Thread.Sleep(1);
+                            }
+                        });
+                    }
+                }
+                OutputMsg("Run count: " + runCount);
+                OutputMsg("WaitingWorkCount: " + powerPool.WaitingWorkCount);
+                if (runCount != powerPool.WaitingWorkCount)
+                {
+                    if (runCount != powerPool.WaitingWorkCount)
+                    {
+                        OutputMsg("error0: runCount != powerPool.WaitingWorkCount");
+                    }
+                    break;
+                }
 
-                await Task.WhenAll(tasks);
+                powerPool.Start();
+                OutputMsg("Running... AliveWorkerCount: " + powerPool.AliveWorkerCount + " | RunningWorkerCount: " + powerPool.RunningWorkerCount + " | IdleWorkerCount: " + powerPool.IdleWorkerCount);
 
-                await powerPool.WaitAsync();
+                int r1 = random.Next(0, 101);
+                if (r1 >= 81 && r1 <= 100)
+                {
+                    OutputMsg("Stopping...");
+                    powerPool.Stop();
+                    await powerPool.WaitAsync();
+                    OutputMsg("AliveWorkerCount: " + powerPool.AliveWorkerCount + " | RunningWorkerCount: " + powerPool.RunningWorkerCount);
+                    OutputMsg("IdleWorkerCount: " + powerPool.IdleWorkerCount);
+                    OutputMsg("WaitingWorkCount: " + powerPool.WaitingWorkCount);
+                    OutputMsg("FailedWorkCount: " + powerPool.FailedWorkCount);
+                    OutputMsg("DoneCount: " + doneCount);
+                    if (powerPool.RunningWorkerCount > 0 || powerPool.WaitingWorkCount > 0)
+                    {
+                        if (powerPool.RunningWorkerCount > 0)
+                        {
+                            OutputMsg("error1: powerPool.RunningWorkerCount > 0" + " - " + powerPool.RunningWorkerCount);
+                        }
+                        if (powerPool.WaitingWorkCount > 0)
+                        {
+                            OutputMsg("error1: powerPool.WaitingWorkCount > 0" + " - " + powerPool.WaitingWorkCount);
+                        }
+                        break;
+                    }
+                }
+                else if (r1 >= 61 && r1 <= 80)
+                {
+                    OutputMsg("Force Stopping...");
+                    powerPool.Stop(true);
+                    await powerPool.WaitAsync();
+                    OutputMsg("AliveWorkerCount: " + powerPool.AliveWorkerCount + " | RunningWorkerCount: " + powerPool.RunningWorkerCount);
+                    OutputMsg("IdleWorkerCount: " + powerPool.IdleWorkerCount);
+                    OutputMsg("WaitingWorkCount: " + powerPool.WaitingWorkCount);
+                    OutputMsg("FailedWorkCount: " + powerPool.FailedWorkCount);
+                    OutputMsg("DoneCount: " + doneCount);
+                    if (powerPool.RunningWorkerCount > 0 || powerPool.WaitingWorkCount > 0)
+                    {
+                        if (powerPool.RunningWorkerCount > 0)
+                        {
+                            OutputMsg("error2: powerPool.RunningWorkerCount > 0" + " - " + powerPool.RunningWorkerCount);
+                        }
+                        if (powerPool.WaitingWorkCount > 0)
+                        {
+                            OutputMsg("error2: powerPool.WaitingWorkCount > 0" + " - " + powerPool.WaitingWorkCount);
+                        }
+                        break;
+                    }
+                }
+                else
+                {
+                    OutputMsg("Waiting...");
+                    await powerPool.WaitAsync();
+                    OutputMsg("AliveWorkerCount: " + powerPool.AliveWorkerCount + " | RunningWorkerCount: " + powerPool.RunningWorkerCount);
+                    OutputMsg("IdleWorkerCount: " + powerPool.IdleWorkerCount);
+                    OutputMsg("WaitingWorkCount: " + powerPool.WaitingWorkCount);
+                    OutputMsg("FailedWorkCount: " + powerPool.FailedWorkCount);
+                    OutputMsg("DoneCount: " + doneCount);
+                    if (powerPool.RunningWorkerCount > 0 || powerPool.WaitingWorkCount > 0 || runCount != doneCount)
+                    {
+                        if (powerPool.RunningWorkerCount > 0)
+                        {
+                            OutputMsg("error3: powerPool.RunningWorkerCount > 0" + " - " + powerPool.RunningWorkerCount);
+                        }
+                        if (powerPool.WaitingWorkCount > 0)
+                        {
+                            OutputMsg("error3: powerPool.WaitingWorkCount > 0" + " - " + powerPool.WaitingWorkCount);
+                        }
+                        if (runCount != doneCount)
+                        {
+                            OutputMsg("error3: runCount != doneCount");
+                        }
+                        break;
+                    }
+                }
+                OutputMsg("---------------");
+                int r2 = random.Next(0, 101);
+                if (r1 >= 81 && r1 <= 100)
+                {
+                }
+                else
+                {
+                    Sleep(random.Next(0, 1500));
+                }
             }
             OutputMsg("done");
 
