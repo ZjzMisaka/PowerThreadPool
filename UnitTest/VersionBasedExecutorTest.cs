0a5dbf748b2a3ee96998d681de34d342e658cac1
ZjzMisaka <zjzmisaka@outlook.com>
2024-08-04 01:46:10 +0800

    [+] VersionBasedExecutorTest

---
+++ UnitTest/VersionBasedExecutorTest.cs
@@ -0,0 +1,27 @@
+﻿using PowerThreadPool.Helpers;
+using System.Reflection;
+
+namespace UnitTest
+{
+    public class VersionBasedExecutorTest
+    {
+        [Fact]
+        public void TestVersionBasedExecutorMaxValue()
+        {
+            bool actionCalled = false;
+            Action action = () => actionCalled = true;
+            var executor = new VersionBasedExecutor(action);
+
+            FieldInfo updatedVersionField = typeof(VersionBasedExecutor)
+                .GetField("_updatedVersion", BindingFlags.NonPublic | BindingFlags.Instance);
+            updatedVersionField.SetValue(executor, long.MaxValue);
+
+            executor.UpdateVersion();
+
+            long updatedVersion = (long)updatedVersionField.GetValue(executor);
+            Assert.Equal(long.MinValue + 1, updatedVersion);
+
+            Assert.False(actionCalled);
+        }
+    }
+}
