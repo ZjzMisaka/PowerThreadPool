809eb84f3303fa91095ac06fb0b560a422d5a82a
ZjzMisaka <530514257@qq.com>
2025-02-18 16:58:16 +0800

    [+] DeferredActionTimer

---
+++ PowerThreadPool/Utils/DeferredActionTimer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading;
+
+internal class DeferredActionTimer : IDisposable
+{
+    private Timer _timer;
+
+    internal DeferredActionTimer(Action action)
+    {
+        _timer = new Timer(_ =>
+        {
+            _timer.Change(Timeout.Infinite, Timeout.Infinite);
+            action.Invoke();
+        });
+    }
+
+    internal void Set(int milliseconds)
+    {
+        _timer.Change(milliseconds, Timeout.Infinite);
+    }
+
+    internal void Cancel()
+    {
+        _timer.Change(Timeout.Infinite, Timeout.Infinite);
+    }
+
+    public void Dispose()
+    {
+        _timer.Dispose();
+    }
+}
