7c1e5c02853103aab3c894ba6ce436e72cc591e0
ZjzMisaka <530514257@qq.com>
2025-06-16 10:42:51 +0800

    [+] PowerThreadPool.QueueWorkItemAsync.cs

---
+++ PowerThreadPool/Core/PowerThreadPool.QueueWorkItemAsync.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using PowerThreadPool.Collections;
+using PowerThreadPool.Helpers;
+using PowerThreadPool.Options;
+using PowerThreadPool.Results;
+using PowerThreadPool.Utils;
+using PowerThreadPool.Works;
+
+namespace PowerThreadPool
+{
+    public partial class PowerPool
+    {
+        private void PrepareAsyncWork<T>(WorkOption<T> option)
+        {
+            CheckPowerPoolOption();
+
+            option.AsyncWorkID = CreateID(option);
+            option.BaseAsyncWorkID = option.AsyncWorkID;
+            option.AllowEventsAndCallback = false;
+
+            _asyncWorkIDDict[option.AsyncWorkID] = new ConcurrentSet<string>();
+        }
+
+        private static void ThrowInnerIfNeeded(Task task)
+        {
+            if (task.Exception?.InnerException != null)
+            {
+                throw task.Exception.InnerException;
+            }
+        }
+
+        private void RegisterCompletion(Task task, SynchronizationContext prevCtx, string baseAsyncWorkId)
+        {
+            task.ContinueWith(_ =>
+            {
+                SynchronizationContext.SetSynchronizationContext(prevCtx);
+                _asyncWorkIDDict.TryRemove(baseAsyncWorkId, out ConcurrentSet<string> set);
+                CheckPoolIdle();
+            });
+        }
+
+        public string QueueWorkItemAsync(Func<Task> asyncFunc, Action<ExecuteResult<object>> callBack = null)
+        {
+            WorkOption workOption = new WorkOption { Callback = callBack };
+            return QueueWorkItemAsync(asyncFunc, workOption);
+        }
+
+        public string QueueWorkItemAsync(Func<Task> asyncFunc, WorkOption option)
+        {
+            PrepareAsyncWork(option);
+
+            return QueueWorkItem(() =>
+            {
+                var prevCtx = SynchronizationContext.Current;
+                var ctx = new PowerPoolSynchronizationContext(this, option);
+                SynchronizationContext.SetSynchronizationContext(ctx);
+
+                Task task = asyncFunc();
+                ThrowInnerIfNeeded(task);
+
+                ctx.SetTask(task);
+                RegisterCompletion(task, prevCtx, option.BaseAsyncWorkID);
+            }, option);
+        }
+
+        public string QueueWorkItemAsync<TResult>(Func<Task<TResult>> asyncFunc, Action<ExecuteResult<TResult>> callBack = null)
+        {
+            WorkOption<TResult> workOption = new WorkOption<TResult> { Callback = callBack };
+            return QueueWorkItemAsync(asyncFunc, workOption);
+        }
+
+        public string QueueWorkItemAsync<TResult>(Func<Task<TResult>> asyncFunc,
+                                                  WorkOption<TResult> option)
+        {
+            PrepareAsyncWork(option);
+
+            return QueueWorkItem(() =>
+            {
+                var prevCtx = SynchronizationContext.Current;
+                var ctx = new PowerPoolSynchronizationContext<TResult>(this, option);
+                SynchronizationContext.SetSynchronizationContext(ctx);
+
+                Task task = asyncFunc();
+                ThrowInnerIfNeeded(task);
+
+                ctx.SetTask(task);
+                RegisterCompletion(task, prevCtx, option.BaseAsyncWorkID);
+
+                return default;
+            }, option);
+        }
+    }
+}
--- PowerThreadPool/Utils/PowerPoolSynchronizationContext.cs
+++ PowerThreadPool/Utils/PowerPoolSynchronizationContext.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Threading;
+﻿using System.Threading;
 using System.Threading.Tasks;
 using PowerThreadPool.Collections;
 using PowerThreadPool.Options;
