d5686bdaa8119e8b427bbe85be8d250a0deb467e
ZjzMisaka <530514257@qq.com>
2025-11-18 12:31:12 +0800

    [-] remove unused using

--- PowerThreadPool/Core/PowerThreadPool.Parallel.cs
+++ PowerThreadPool/Core/PowerThreadPool.Parallel.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
-using System.Threading;
 using System.Threading.Tasks;
 using PowerThreadPool.Collections;
 using PowerThreadPool.Constants;
