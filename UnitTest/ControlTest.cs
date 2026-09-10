81e0e49af626a0440f81c28d46442511ea807ab1
ZjzMisaka <zjzmisaka@outlook.com>
2026-09-11 06:12:42 +0800

    fix: test TestPauseTwiceByID

--- UnitTest/ControlTest.cs
+++ UnitTest/ControlTest.cs
@@ -342,8 +342,8 @@ namespace UnitTest
                 item => Assert.Equal("Work0 END", item)
             );
 
-            Assert.InRange(d1, 1999, 2050);
-            Assert.InRange(t1, 1999, 2050);
+            Assert.InRange(d1, 1999, 2300);
+            Assert.InRange(t1, 1999, 2300);
         }
 
         [Fact]
